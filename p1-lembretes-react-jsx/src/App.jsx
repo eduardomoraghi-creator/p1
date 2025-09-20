35d0e787ec4c365203b2c5d757d126bb36aae289
Eduardo Moraghi <eduardomoraghi@gmail.com>
2025-09-20 17:31:05 -0300

    feat(p1): cria componente principal e faz hello

---
+++ p1-lembretes-react-jsx/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container text-primary-emphasis text-center fs-5
+         border border-primary-subtle rounded-3">
+      <div className="row">
+        <div className="col-12 col-lg-6 col-xxl-4">
+          Hello, Lembretes
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
